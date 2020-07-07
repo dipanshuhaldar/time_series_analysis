33bab4bfff76da2e7669dfbb0b3535cd7ebafc55
dipanshuhaldar <dipanshuhaldar@gmail.com>
2020-07-07 10:52:04 +0000

    automatic commit

--- time_series_analysis_R.R
+++ time_series_analysis_R.R
@@ -1,6 +1,9 @@
 ################# Time Series Analysis ##################
 data("AirPassengers")
 AP = AirPassengers
+class(AP)#AP is already a ts object
+#what is a ts object?
+start(AP); end(AP); frequency(AP)
 
 
 
