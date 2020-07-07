3ba2044eaf610904dff8ac47d146cd0eeeca3496
dipanshuhaldar <dipanshuhaldar@gmail.com>
2020-07-07 06:09:17 +0000

    automatic commit

---
+++ time_series_analysis_R.R
@@ -0,0 +1,17 @@
+################# Time Series Analysis ##################
+print('hello world!')
+
+
+
+
+
+
+#########################################################
+
+#################### Git Commands #######################
+#system('git config --global user.email "dipanshuhaldar@gmail.com"')
+#system('git config --global user.name "dipanshuhaldar"')
+
+system('git add .')
+system('git commit -m "automatic commit"')
+system('git push origin master')
