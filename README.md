d1e102bdda20c5468682901263bd85811ae02174
dipanshuhaldar <dipanshuhaldar@gmail.com>
2020-07-09 06:19:54 +0000

    code with md

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # A repository of Exploratory Time Series Analysis and model building in *R* and *Python* 
 
 ### R
-! [Alt text] ('/cloud/project/uploads/R.jpg')
 
 Time Series objects in R are characterised by a 
 * *start*
 * *end*
 * *frequency*
+
+* What is *Trend* in Time Series Data?
+A systematic change in Time Series data which does not appear to be periodic is known as *Trend*
--- time_series_analysis_R.R
+++ time_series_analysis_R.R
@@ -11,12 +11,5 @@ start(AP); end(AP); frequency(AP)
 
 
 
+
 #########################################################
-
-#################### Git Commands #######################
-#system('git config --global user.email "dipanshuhaldar@gmail.com"')
-#system('git config --global user.name "dipanshuhaldar"')
-
-system('git add .')
-system('git commit -m "automatic commit"')
-system('git push origin master')
